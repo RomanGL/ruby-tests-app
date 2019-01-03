ff4b0534b1920a2a1a0d58f827a56bb8d3927e6c
Roman Gladkikh <roman.gladkikh@outlook.com>
2019-01-03 22:34:05 +0700

    Добавлены модели вопроса и ответа на вопрос.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,6 @@
 class Quiz < ApplicationRecord
+  has_many :questions, dependent: :destroy
+
   validates :title, presence: true,
                     length: { minimum: 5 }
   validates :description, presence: true,
