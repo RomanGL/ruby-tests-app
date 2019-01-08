43e41a5d8afd1e8444b8e245667187f784b7bb0b
Roman Gladkikh <roman.gladkikh@outlook.com>
2019-01-09 01:28:22 +0700

    Миграция для добавления поля Name пользователю включена в миграцию по созданию таблицы пользователей.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_161235) do
+ActiveRecord::Schema.define(version: 2019_01_06_170333) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_01_08_161235) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
@@ -47,7 +48,6 @@ ActiveRecord::Schema.define(version: 2019_01_08_161235) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
