a0b553c7c69eae0241aaf1d90f1731abb90bdcd1
Roman Gladkikh <roman.gladkikh@outlook.com>
2019-01-08 21:20:44 +0700

    Добавление ответов на вопрос в модальном окне. Все тексты из приложения вынесены в файл локали ru.yml.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :quizzes, only: [ :index, :new, :edit, :create, :update, :destroy ] do
     resources :questions, only: [ :index, :new, :edit, :create, :update, :destroy ] do
-      resources :answers
+      resources :answers, only: [ :index, :new, :edit, :create, :update, :destroy ]
     end
   end
 
