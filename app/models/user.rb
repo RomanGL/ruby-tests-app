f55859e0ee1b3da75ed6ba9626c3c512eccde65b
Surnak <ee521@mail.ru>
2019-01-09 00:06:54 +0700

    Добавлено поле Name при регистрации
    Добавлено в модель User дополнительное поле Name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :omniauthable #:confirmable, :recoverable, :lockable, :timeoutable, :rememberable, :database_authenticatable,
   devise :registerable, :validatable, :rememberable, :trackable, :database_authenticatable
+
+  validates :name, presence: true
 end
