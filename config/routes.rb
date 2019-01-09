d7b27650d4cd5e3ea09fc3fa0ddbd085ab25e528
Sergey Pashkin <pashkin_sergey@mail.ru>
2019-01-09 23:45:31 +0600

    добавлены контроллеры для 403,404,500 ошибок

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  match "/403", :to => "errors#forbidden", :via => :all
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
   devise_for :users, controllers: {registrations: 'users/registrations'}
   get 'welcome/index'
 
---
+++ test/controllers/errors_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ErrorsControllerTest < ActionDispatch::IntegrationTest
+  test "should get not_found_forbidden_internal_server_error" do
+    get errors_not_found_forbidden_internal_server_error_url
+    assert_response :success
+  end
+
+end
