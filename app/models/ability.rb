d60317427e35f0d6736e927b39d571758c9c9f64
Surnak <ee521@mail.ru>
2019-01-09 21:28:46 +0700

    Добавлено разграничение прав с помощью Cancancan и Rolify
    Роли разграничены на admin, student и guest

---
+++ app/models/ability.rb
@@ -0,0 +1,27 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+#    can :read, Welcome
+#
+#    if user
+#      if user.admin?
+#        can :manage, :all
+#      elsif user.teacher?
+#          can :read, :all
+#	  can [:create, :edit, :destroy], [Quiz, Question, Answer]
+#          cannot :destroy, User, id: user_id
+#      elsif user.student?
+#          can :read, :all  
+#      end
+#    else
+#      can :sign_up
+#    end
+    user ||= User.new # guest user (not logged in)
+    if user.has_role? :admin
+      can :manage, :all
+    elsif user.has_role? :student
+      can :read, :all
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :omniauthable #:confirmable, :recoverable, :lockable, :timeoutable, :rememberable, :database_authenticatable,
   devise :registerable, :validatable, :rememberable, :trackable, :database_authenticatable
 
   validates :name, presence: true
+
+  after_create :assign_default_role
+  def assign_default_role
+    self.add_role(:student) if self.roles.blank?
+  end
+
 end
